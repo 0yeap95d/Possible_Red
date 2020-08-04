87a491860f9b5dfb7a5904b7baedcf6561b22584
oheong <enffl9568@naver.com>
2020-08-04 18:39:16 +0900

    S03P13D201-143 | post Add | 에영

--- frontend/src/components/add/PostAdd.vue
+++ frontend/src/components/add/PostAdd.vue
@@ -1,13 +1,41 @@
 <template>
   <div>
-    <Navbar />
-    <Footer />
+    <div class="form-wrap">
+      <div class="input-with-label">
+        <input
+          v-model="post.postContent"
+          id="postContent"
+          placeholder="내용을 입력하세요"
+          type="text"
+        />
+      </div>
+      <div>
+        <button class="btn-bottom" @click="postRegister">인증하기</button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  created() {
+    console.log(this.$session.get("user").memberNo);
+    this.mission.memberNo = this.$session.get("user").memberNo;
+  },
+  data() {
+    return {
+      post: {
+        memberNo: 0,
+        postPhoto: "",
+        postContent: "",
+        missionNo: 0,
+      },
+    };
+  },
+  methods: {
+    postRegister() {},
+  },
+};
 </script>
 
-<style>
-</style>
+<style></style>
