998edb88a2399505b21564fb09f842955e5294bb
김희정 <limin7769@naver.com>
2020-08-04 16:27:57 +0900

    세번째 정리 footer

--- frontend/src/views/post/Posts.vue
+++ frontend/src/views/post/Posts.vue
@@ -4,18 +4,21 @@
       <Posting />
       <Posting />
       <Posting />
+      <Footer />
   </div>
 </template>
 
 <script>
 import Navbar from '../../components/common/Navbar.vue'
 import Posting from '../../components/common/Posting.vue'
+import Footer from '../../components/common/Footer.vue'
 
 export default {
     name:'Posts',
     components:{
       Navbar,
       Posting,
+      Footer,
     },
 }
 </script>
