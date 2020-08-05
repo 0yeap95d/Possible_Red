26a4c91c0438bcca385f5e4046b1eabac212e1cc
김희정 <limin7769@naver.com>
2020-08-05 17:39:57 +0900

    S03P13D201-149 | Sidebar | 희정

--- frontend/src/components/common/navbar.vue
+++ frontend/src/components/common/navbar.vue
@@ -17,11 +17,20 @@
         </div>
         </template>
         <div class="px-3 py-2">
-            <p>
-            Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
-            in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-            </p>
-            <b-img src="https://picsum.photos/500/500/?image=54" fluid thumbnail></b-img>
+            <div class="thumbnail">
+                <div class="centered">
+                    <img src="../../assets/images/펭수프로필.jpg">
+                </div>
+            </div>
+            <hr>
+            <div>
+                <ul>
+                    <li class="text"><i class="far fa-list-alt"></i>&nbsp;내가 참여한 미션</li>
+                    <li class="text"><i class="fas fa-user-edit"></i>&nbsp;내가 쓴 글</li>
+                    <li class="text"><i class="fas fa-coins"></i>&nbsp;내 포인트</li>
+                </ul>
+            </div>
+            
         </div>
     </b-sidebar>
     </div>
@@ -46,5 +55,39 @@ export default {
     }
     .btn{
         padding:0.125rem 0.25rem;
+    }
+    .thumbnail{
+        position:relative;
+        padding-top:100%; /*1:1 ratio */
+        overflow:hidden;
+    }
+    .thumbnail .centered{
+        position:absolute;
+        top:0;
+        left:0;
+        right:0;
+        bottom:0;
+        -webkit-transform: translate(50%,50%); 
+        -ms-transform: translate(50%,50%); 
+        transform: translate(50%,50%);
+        
+    }
+    .thumbnail .centered img { 
+        position: absolute; 
+        top: 0; 
+        left: 0; 
+        max-width: 100%; 
+        height: 90%; 
+        -webkit-transform: translate(-50%,-50%); 
+        -ms-transform: translate(-50%,-50%); 
+        transform: translate(-50%,-50%); 
+        border-radius:50%;
+        overflow: hidden;
+    }
+    .container > div{
+        padding-top:10px;
+    }
+    .text{
+        margin-bottom:20px;
     }
 </style>
