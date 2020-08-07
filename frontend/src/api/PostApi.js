431253dc028a0dbcd638f10e614a64d04068c0b7
oheong <enffl9568@naver.com>
2020-08-07 15:48:31 +0900

    S03P13D201-143 | post Add | 에영

--- frontend/src/api/PostApi.js
+++ frontend/src/api/PostApi.js
@@ -1,22 +1,39 @@
 import Axios from "axios";
 
-const requestSelectPost = (callback,errorCallback) => {
-    
-    Axios.get('http://localhost:8080/sns201/post/all')
-    .then(res => callback(res))
-    .catch(error => errorCallback(error))
-}
+const requestSelectPost = (callback, errorCallback) => {
+    Axios.get("http://localhost:8080/sns201/post/all")
+        .then((res) => callback(res))
+        .catch((error) => errorCallback(error));
+};
 
-const requestSelectPostByNo = (data,callback,errorCallback) => {
+const requestSelectPostByNo = (data, callback, errorCallback) => {
+    Axios.get("http://localhost:8080/sns201/post" + data)
+        .then((res) => callback(res))
+        .catch((error) => errorCallback(error));
+};
 
-    Axios.get('http://localhost:8080/sns201/post' + data)
-    .then(res => callback(res))
-    .catch(error => errorCallback(error))
-}
+const requestInsertPost = (data, callback, errorCallback) => {
+    var params = {
+        memberNo: data.memberNo,
+        postPhoto: data.postPhoto,
+        postContent: data.postContent,
+        missionNo: data.missionNo,
+    };
+
+    console.log("post" + data);
+
+    Axios.post("http://localhost:8080/sns201/post", params)
+        .then((res) => callback(res))
+        .catch((error) => errorCallback(error));
+};
 
 const PostApi = {
-    requestSelectPost:(data,callback,errorCallback)=>requestSelectPost(data,callback,errorCallback),
-    requestSelectPostByNo:(data,callback,errorCallback)=>requestSelectPostByNo(data,callback,errorCallback)
-}
+    requestSelectPost: (data, callback, errorCallback) =>
+        requestSelectPost(data, callback, errorCallback),
+    requestSelectPostByNo: (data, callback, errorCallback) =>
+        requestSelectPostByNo(data, callback, errorCallback),
+    requestInsertPost: (data, callback, errorCallback) =>
+        requestInsertPost(data, callback, errorCallback),
+};
 
-export default PostApi
+export default PostApi;
--- frontend/src/components/add/PostAdd.vue
+++ frontend/src/components/add/PostAdd.vue
@@ -3,14 +3,17 @@
   <div>
     <div class="form-wrap">
       <!-- 여기에 v-for써서 미션리스트 셀렉트박스로 보여주기 -->
-      <select v-model="post.missionNo" id="missionNo">
-        <option disabled value>게시글을 올릴 미션을 선택하세요.</option>
-        <option
-          v-for="item in missionList"
-          :key="item.etag"
-          :value="item.missionNo"
-        >{{item.missionTitle}}</option>
-      </select>
+      <p>
+        게시글을 올릴 미션을 선택하세요 →
+        <select v-model="post.missionNo" id="missionNo">
+          <option disabled selected>게시글을 올릴 미션을 선택하세요.</option>
+          <option
+            v-for="item in missionList"
+            :key="item.etag"
+            :value="item.missionNo"
+          >{{item.missionTitle}}</option>
+        </select>
+      </p>
       <br />
 
       <div>
@@ -39,7 +42,8 @@
 </template>
 
 <script>
-import MemberApi from "../../api/MemberApi";
+import MemberApi from "../../api/MemberApi"; // 멤버넘버별 포스트 받아오기
+import PostApi from "../../api/PostApi";
 export default {
   created() {
     console.log("포스트 " + this.$session.get("user").memberNo);
@@ -70,7 +74,29 @@ export default {
   },
   methods: {
     postRegister() {
-      console.log(this.post);
+      //console.log(this.post);
+      //console.log(this.post.missionNo);
+      //console.log(this.post.postContent);
+      //console.log(this.post.postPhoto);
+      if (this.post.missionNo === 0) {
+        alert("미션을 입력하세요!");
+        return;
+      } else if (!this.post.postPhoto) {
+        alert("사진을 입력하세요!");
+        return;
+      } else if (!this.post.postContent) {
+        alert("내용을 입력하세요!");
+        return;
+      }
+      console.log("이까지는 들어옴?");
+      PostApi.requestInsertPost(
+        this.post,
+        (res) => {
+          console.log("포스트 등록완료!");
+          this.$router.push("/add");
+        },
+        (error) => {}
+      );
     },
 
     $fileSelect: function fileSelect() {
