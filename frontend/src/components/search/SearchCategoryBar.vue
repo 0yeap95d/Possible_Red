ed34deb438fd632b72872ab26a88ae79c14e6634
hsolk <tashu3101@gmail.com>
2020-08-10 18:27:03 +0900

    S03P13D201-187 | category 별 mission data 받기 | 강해솔

--- frontend/src/components/search/SearchCategoryBar.vue
+++ frontend/src/components/search/SearchCategoryBar.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="text-center search-category">
-    <v-btn small v-for="item in items" :key="item.categoryNo" class="ma-2 mx-1" outlined color="indigo">{{item.categoryContent}}</v-btn>
+    <v-btn small v-for="item in items" :key="item.categoryNo" class="ma-2 mx-1" outlined color="indigo" @click="searchByCategory(item.categoryContent)">{{item.categoryContent}}</v-btn>
   </div>
 </template>
 
@@ -17,7 +17,7 @@ export default {
     getCategoryList() {
       SearchApi.requestCategory(
         (res) => {
-          console.log(res.data)
+          //console.log(res.data)
           this.items = res.data
         },
         (error) => {
@@ -25,6 +25,12 @@ export default {
         }
       );
     },
+    searchByCategory(category) {
+      this.$router.push({
+        name: "SearchByCategory",
+        params: { category: category},
+      });
+    }
   },
   created() {
     this.getCategoryList()
---
+++ frontend/src/views/search/SearchByCategory.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <v-btn small v-for="item in items" :key="item.missionNo" class="ma-2 mx-1" outlined color="indigo">{{item.missionTitle}}</v-btn>
+  </div>
+</template>
+
+<script>
+import SearchApi from "../../api/SearchApi.js";
+
+export default {
+    name: "SearchByCategory",
+    data() {
+      return {
+        category: String,
+        items: [],
+      }
+    },
+    method: {
+
+    },
+    created() {
+      this.category = this.$route.params.category;
+      console.log(this.category)
+
+      SearchApi.requestMissionByCategory(
+        this.category,
+        (res) => {
+          this.items = res.data
+        },
+        (error) => {
+          console.error(error)
+        })
+    },
+}
+</script>
+
+<style>
+
+</style>
