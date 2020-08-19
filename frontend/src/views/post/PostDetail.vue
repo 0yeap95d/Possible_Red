6d81c16c87ed6cd8c11746a6223246dcf35b37c1
김희정 <limin7769@naver.com>
2020-08-20 01:39:00 +0900

    S03P13D201-236 | post 수정 | 희정

--- frontend/src/views/post/PostDetail.vue
+++ frontend/src/views/post/PostDetail.vue
@@ -3,7 +3,7 @@
     <v-app>
       <v-card class="mx-0 overflow-hidden missions" color="white">
         <v-app-bar color="deep-purlple" dark>
-          <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+          <HeaderComponent :isBack="true" />
 
           <v-toolbar-title>POST_DETAIL</v-toolbar-title>
         </v-app-bar>
@@ -16,7 +16,7 @@
           <v-card class="mx-auto">
             <v-list-item>
               <v-list-item-avatar>
-                <v-img src="../../assets/images/background1.jpg"></v-img>
+                <v-img :src="profileImagePath"></v-img>
               </v-list-item-avatar>
               <v-list-item-content>
                 <v-list-item-title class="headline">by {{member.nickname}}</v-list-item-title>
@@ -26,7 +26,7 @@
             <div>
               <div class="thumbnail">
                 <div class="centered">
-                  <img :src="imagePath" />
+                  <v-img :src="imagePath" class="min_size" ></v-img>
                 </div>
               </div>
             </div>
@@ -84,56 +84,9 @@
         
           <PostComment @removeComment="updateComment" @updateComment="updateComment" :postNo="postOne.postNo" :comment-lists="comments" />
         </div>
-
-        <v-navigation-drawer v-model="drawer" absolute temporary>
-          <v-list nav dense>
-            <v-list-item-group active-class="deep-purple--text text--accent-4">
-              <div class="px-3 py-2">
-                <div class="thumbnail">
-                  <div class="centered">
-                    <img :src="profileImagePath" />
-                  </div>
-                </div>
-              </div>
-              <hr />
-              <v-list-item @click="mymission">
-                <v-list-item-icon>
-                  <i class="far fa-list-alt"></i>
-                </v-list-item-icon>
-                <p class="jua">내가 참여한 미션</p>
-              </v-list-item>
-
-              <v-list-item @click="mypost">
-                <v-list-item-icon>
-                  <i class="fas fa-user-edit"></i>
-                </v-list-item-icon>
-                <p class="jua">내가 쓴 글</p>
-              </v-list-item>
-
-              <v-list-item @click="mypoint">
-                <v-list-item-icon>
-                  <i class="fas fa-coins"></i>
-                </v-list-item-icon>
-                <p class="jua">내 포인트</p>
-              </v-list-item>
-
-              <v-list-item @click="myaccount">
-                <v-list-item-icon>
-                  <i class="fas fa-users-cog"></i>
-                </v-list-item-icon>
-                <p class="jua">내 계정설정</p>
-              </v-list-item>
-
-              <v-list-item @click="logout">
-                <v-list-item-icon>
-                  <i class="fas fa-sign-out-alt"></i>
-                </v-list-item-icon>
-                <p class="jua">로그아웃</p>
-              </v-list-item>
-            </v-list-item-group>
-          </v-list>
-        </v-navigation-drawer>
       </v-card>
+
+
       <v-bottom-navigation black shift>
         <v-btn @click="post">
           <span>POST</span>
@@ -174,6 +127,7 @@ import CommentApi from "../../api/CommentApi";
 import UserApi from "../../api/UserApi";
 import LikeApi from "../../api/LikeApi";
 import HashtagApi from "../../api/HashtagApi";
+import HeaderComponent from "../../components/common/Header.vue";
 
 export default {
   data: () => ({
@@ -275,6 +229,7 @@ export default {
 
   components: {
     PostComment,
+    HeaderComponent,
   },
   methods: {
     returnpost() {
@@ -354,18 +309,6 @@ export default {
     profile() {
       this.$router.push("/profile");
     },
-    mymission() {
-      this.$router.push("/mymission");
-    },
-    mypost() {
-      this.$router.push("/mypost");
-    },
-    mypoint() {
-      this.$router.push("/mypoint");
-    },
-    myaccount() {
-      this.$router.push("/changeuser");
-    },
     getComments(num) {
       CommentApi.requestGetAllComment(
         num,
@@ -389,32 +332,6 @@ export default {
 <style scoped>
 .theme--dark.v-app-bar.v-toolbar.v-sheet {
   background: linear-gradient(to left, #f48fb1, #3949ab);
-}
-.thumbnail {
-  position: relative;
-  padding-top: 100%; /*1:1 ratio */
-  overflow: hidden;
-}
-.thumbnail .centered {
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  -webkit-transform: translate(50%, 50%);
-  -ms-transform: translate(50%, 50%);
-  transform: translate(50%, 50%);
-}
-.thumbnail .centered img {
-  position: absolute;
-  top: 0;
-  left: 0;
-  max-width: 100%;
-  height: 90%;
-  -webkit-transform: translate(-50%, -50%);
-  -ms-transform: translate(-50%, -50%);
-  transform: translate(-50%, -50%);
-  overflow: hidden;
 }
 .v-bottom-navigation {
   position: fixed;
@@ -444,7 +361,10 @@ export default {
 .v-toolbar__title {
   font-family: "Luckiest Guy", cursive;
   font-size: x-large;
+  margin: 0 0 0 20px;
 }
-
+.min_size {
+  min-width: 328px;
+}
 
 </style>
