845eca9cf8701c1572e30dbcb4c1640c43edfd2b
tjdwob <tjdwob@gmail.com>
2020-08-20 18:42:35 +0900

    S03P13D201-270 | Frontend 내 포스트 페이지 수정 | 배성재

--- frontend/src/components/my/post.vue
+++ frontend/src/components/my/post.vue
@@ -6,6 +6,7 @@
             <v-card>
               <v-list two-line>
                 <template v-for="item in items">
+                  <div v-if="item.cnt != 0" :key="item.cnt"> 
                   <v-subheader
                     :key="item.missionTitle"
                     inset
@@ -14,27 +15,28 @@
                   </v-subheader>
                     
                   <!--여기서 부터는 그 미션에 대한 포스터들  그래서 지우고 다시-->
-                  <div v-for="(post, i) in item.post" :key="i">
-                    <div v-if="item.post[i].memberNo == user.memberNo">
-                      <v-list-item
-                        ripple
-                        @click="postdetail(item.post[i].postNo)"
-                      >
-                        
-                        <v-list-item-avatar>
-                          <img :src="item.post[i].postPhoto">
-                        </v-list-item-avatar>
-                        
-                        <v-list-item-content>
-                            <v-list-item-title v-html="item.post[i].postContent" class="jua"></v-list-item-title>
-                        </v-list-item-content>
-                        
-                      </v-list-item>
-                      <hr>
-                      <br>
+                  
+                    <div v-for="(post, i) in item.post" :key="i">
+                        <div v-if="item.cnt != 0" :key="item.cnt">
+                          <div v-if="post.memberNo == user.memberNo">
+                            <v-list-item
+                              ripple
+                              @click="postdetail(post.postNo)"
+                            >
+                              <v-list-item-avatar>
+                                <img :src="post.postPhoto">
+                              </v-list-item-avatar>
+                              
+                              <v-list-item-content>
+                                  <v-list-item-title v-html="post.postContent" class="jua"></v-list-item-title>
+                              </v-list-item-content>
+                              
+                            </v-list-item>
+                            <hr><br>
+                          </div>
+                        </div>
                     </div>
                   </div>
-                    
                 </template>
               </v-list>
             </v-card>
@@ -55,30 +57,7 @@
     },
     created(){
       this.user = this.$session.get("user");
-
-      let pos = 0;
-      for (var i in this.missions) {
-        console.log(this.missions[pos].missionTitle)
-        PostApi.requestPostByMission(
-          this.missions[i].missionNo,
-          (res) => {
-            let item = {
-              missionTitle: this.missions[pos++].missionTitle,
-              post: res.data,
-            }
-            for (var j in item.post) {
-              this.imageSplit = item.post[j].postPhoto.split("/");
-              this.index = this.imageSplit.length - 1;
-              item.post[j].postPhoto = this.imagePath + this.imageSplit[this.index];
-            }
-            console.log(item);
-            this.items.push(item)
-          },
-          (error) => { console.log(error) }
-        )
-      }
-
-      
+      this.processArray(this.missions);
     },
     data () {
       return {
@@ -88,6 +67,7 @@
         imagePath: "http://i3d201.p.ssafy.io:8080/",
         index: 0,
         imageSplit: [],
+      
       }
     },
     methods:{
@@ -99,6 +79,33 @@
           },
         });
       },
+
+      async processArray(missions) {
+        for (const mission of missions) {
+          await PostApi.requestPostByMission(
+            mission.missionNo,
+            (res) => {
+              let item = {
+                missionTitle: mission.missionTitle,
+                post: res.data,
+                cnt: 0,
+              }
+
+              for (var p of item.post) {
+                if (this.user.memberNo == p.memberNo) item.cnt++;
+              }
+              
+              for (var j in item.post) {
+                this.imageSplit = item.post[j].postPhoto.split("/");
+                this.index = this.imageSplit.length - 1;
+                item.post[j].postPhoto = this.imagePath + this.imageSplit[this.index];
+              }
+              this.items.push(item);
+            },
+            (error) => { console.log(error) }
+          )
+        }
+      }
     }
   }
 </script>
